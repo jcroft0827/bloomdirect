49ff328a999a35a52c8d8e0ef2a3e0757c77aed2
Joseph Croft <jcroft0827@gmail.com>
2026-02-18 14:56:52 -0500

    Switched production environment to look at new production database

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -4,6 +4,14 @@ let isConnected = false;
 
 export const connectToDB = async () => {
   if (isConnected) return;
-  await mongoose.connect(process.env.MONGODB_URI!);
+  // await mongoose.connect(process.env.MONGODB_URI!);
+  const MONGODB_URI = 
+    process.env.NODE_ENV === "production"
+      ? process.env.MONGODB_URI
+      : process.env.MONGODB_URI_DEV;
+    if (!MONGODB_URI) {
+      throw new Error("MONGODB_URI is not defined");
+    }
+  await mongoose.connect(MONGODB_URI!);
   isConnected = true;
 };
